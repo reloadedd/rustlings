1fdbd32bd4390f35f10c9b5ce30e03d93294e2fd
reloadedd <reloadedd@protonmail.com>
2022-06-18 00:25:04 +0300

    Complete Enum section

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -1,10 +1,12 @@
 // enums3.rs
 // Address all the TODOs to make the tests pass!
 
-// I AM NOT DONE
-
 enum Message {
     // TODO: implement the message variant types based on their usage below
+    ChangeColor((u8, u8, u8)),
+    Echo(String),
+    Move(Point),
+    Quit
 }
 
 struct Point {
@@ -37,6 +39,12 @@ impl State {
 
     fn process(&mut self, message: Message) {
         // TODO: create a match expression to process the different message variants
+        match message {
+            Message::ChangeColor((u1, u2, u3)) => self.change_color((u1, u2, u3)),
+            Message::Echo(str) => self.echo(str),
+            Message::Move(position) => self.move_position(position),
+            Message::Quit => self.quit()
+        }
     }
 }
 
